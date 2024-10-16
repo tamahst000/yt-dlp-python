3a6a7ee00d326b164fdc8b85204ead362c016284
tamahst <tamahst000@gmail.com>
2024-10-16 17:00:34 +0900

    ファイル名置換処理

--- py/pj_sekai_mv_mp3.py
+++ py/pj_sekai_mv_mp3.py
@@ -1,38 +1,7 @@
 import os
 from yt_dlp import YoutubeDL
-from PIL import Image
-from mutagen.id3 import ID3, APIC
 
-
-def convert_thumbnail(webp_path, convert_path):
-    if not os.path.exists(webp_path):
-        print("----- ERROR : 画像ファイルが見つかりません。")
-        return
-    with Image.open(webp_path) as img:
-        img.save(convert_path)
-    os.remove(webp_path)
-    print("----- Thumbnail convert completed!")
-
-
-def embed_image_in_mp3(audio_path, img_path):
-    if not os.path.exists(audio_path):
-        print("----- ERROR : 音楽ファイルが見つかりません。")
-        return
-
-    tags = ID3(audio_path)
-    with open(img_path, "rb") as img_file:
-        tags.add(
-            APIC(
-                mime="image/jpeg",
-                type=3,
-                desc="Cover",
-                data=img_file.read(),
-            )
-        )
-    tags.save()
-    os.remove(img_path)
-    print("----- Thumbnail embedding completed!")
-
+from utils import get_url_list, convert_thumbnail, embed_image_in_mp3, sanitize_filename
 
 cookies_file = "cookies.txt"
 output_dir = "yt-dlp"
@@ -50,16 +19,7 @@ ydl_opts = {
     ],
 }
 
-url_list = []
-with open(r".\\url.txt", "r", encoding="utf_8") as f:
-    while True:
-        line = f.read()
-        script_line = line.split("\n")
-        for t in script_line:
-            if "http" in t:
-                url_list.append(t)
-        else:
-            break
+url_list = get_url_list()
 
 for inURL in url_list:
     with YoutubeDL(ydl_opts) as ydl:
@@ -73,3 +33,9 @@ for inURL in url_list:
 
     audio_path = os.path.join(output_dir, f"{filename_base}.mp3")
     embed_image_in_mp3(audio_path, thumbnail_path)
+
+    sanitized_filename, change = sanitize_filename(filename_base)
+    if change:
+        new_audio_path = os.path.join(output_dir, f"{sanitized_filename}.mp3")
+        os.rename(audio_path, new_audio_path)
+        print("-----Update filename!")
--- py/ym_txt.py
+++ py/ym_txt.py
@@ -1,47 +1,7 @@
 import os
 from yt_dlp import YoutubeDL
-from PIL import Image
-from mutagen.id3 import ID3, APIC
 
-
-def crop_thumbnail(webp_path, crop_path):
-    if not os.path.exists(webp_path):
-        print("----- ERROR : 画像ファイルが見つかりません。")
-        return
-
-    with Image.open(webp_path) as img:
-        img_width, img_height = img.size
-        target_width, target_height = img_height, img_height
-
-        left = (img_width - target_width) // 2
-        upper = (img_height - target_height) // 2
-        right = left + target_width
-        lower = upper + target_height
-
-        cropped_img = img.crop((left, upper, right, lower))
-        cropped_img.save(crop_path)
-    os.remove(webp_path)
-    print("----- Thumbnail crop completed!")
-
-
-def embed_image_in_mp3(audio_path, img_path):
-    if not os.path.exists(audio_path):
-        print("----- ERROR : 音楽ファイルが見つかりません。")
-        return
-
-    tags = ID3(audio_path)
-    with open(img_path, "rb") as img_file:
-        tags.add(
-            APIC(
-                mime="image/jpeg",
-                type=3,
-                desc="Cover",
-                data=img_file.read(),
-            )
-        )
-    tags.save()
-    os.remove(img_path)
-    print("----- Thumbnail embedding completed!")
+from utils import get_url_list, crop_thumbnail, embed_image_in_mp3, sanitize_filename
 
 
 cookies_file = "cookies.txt"
@@ -60,16 +20,7 @@ ydl_opts = {
     ],
 }
 
-url_list = []
-with open(r".\\url.txt", "r", encoding="utf_8") as f:
-    while True:
-        line = f.read()
-        script_line = line.split("\n")
-        for t in script_line:
-            if "http" in t:
-                url_list.append(t)
-        else:
-            break
+url_list = get_url_list()
 
 for inURL in url_list:
     if "music" in inURL:
@@ -88,3 +39,9 @@ for inURL in url_list:
 
     audio_path = os.path.join(output_dir, f"{filename_base}.mp3")
     embed_image_in_mp3(audio_path, thumbnail_crop_path)
+
+    sanitized_filename, change = sanitize_filename(filename_base)
+    if change:
+        new_audio_path = os.path.join(output_dir, f"{sanitized_filename}.mp3")
+        os.rename(audio_path, new_audio_path)
+        print("-----Update filename!")
